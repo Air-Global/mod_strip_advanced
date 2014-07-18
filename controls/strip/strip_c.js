f4394d2d881b78c813ca8390162c93975b34a710
MJT Rijntjes <martin@air-global.nl>
2014-07-18 18:40:35 +0200

    2 lines of tabs next to the currently open tab
    Preparation to scroll through tabs

--- controls/strip/strip_c.js
+++ controls/strip/strip_c.js
@@ -7,6 +7,7 @@
  * @author: [mod_strip_advanced] air-global
  * 
  * @log:
+ * - 2014-07-18 air-global 2 lines of unopened tabs & prep for scrolling through tabs
  * - 2014-07-18 air-global rebrand to mod_strip_advanced
  * - 2014-07-08 spolu  Disable actions when not available #11
  * - 2014-06-16 spolu  Tabs filtering
@@ -34,9 +35,14 @@ var strip_c = function(spec, my) {
   my.tabs_el = my.strip_el.find('.tabs');
   my.back_el = my.strip_el.find('.command.back');
   my.forward_el = my.strip_el.find('.command.forward');
+  my.new_el = my.strip_el.find('.command.new');
+  my.scroll_back_el = my.strip_el.find('.command.scroll_back');
+  my.scroll_forward_el = my.strip_el.find('.command.scroll_forward');
 
   my.TAB_WIDTH = 170;
+  my.TAB_HEIGHT = 23;
   my.TAB_MARGIN = 0;
+  my.TAB_SCROLLOFFSET = 0;
 
   /* Dictionary of tabs div elements. */
   my.tabs_divs = {};
@@ -54,6 +60,8 @@ var strip_c = function(spec, my) {
   var cmd_forward;        /* cmd_forward(); */
   var cmd_new;            /* cmd_new(); */
   var cmd_update;         /* cmd_update(); */
+  var cmd_scroll_back;     /* cmd_scroll_back(); */
+  var cmd_scroll_forward;  /* cmd_scroll_forward(); */
 
   var init;               /* init(); */
 
@@ -64,6 +72,8 @@ var strip_c = function(spec, my) {
   var update_tab;         /* update_tab(tab_id, data); */
   var position_tab;       /* update_tab(tab_id, idx); */
   var remove_tab;         /* update_tab(tab_id); */
+  
+  var update_scroll;      /* update_scroll(); */
 
   var mousewheel_handler; /* mousewheel_handler(evt); */
   var dblclick_handler;   /* dblclick_handler(evt); */
@@ -277,7 +287,27 @@ var strip_c = function(spec, my) {
   // ```
   position_tab = function(tab_id, idx) {
     var tab = my.tabs_divs[tab_id];
-    tab.css('left', idx * (my.TAB_WIDTH + my.TAB_MARGIN));
+    var left = (idx/2 * my.TAB_WIDTH + my.TAB_MARGIN) + my.TAB_SCROLLOFFSET;
+    //var left = (idx/2 * my.TAB_WIDTH + my.TAB_MARGIN);
+    var top = my.TAB_HEIGHT;
+    
+    if (idx %2 != 0) {
+		top = 0;
+		left = (idx - 1)/2 * my.TAB_WIDTH + (my.TAB_MARGIN * 2) + my.TAB_WIDTH + my.TAB_SCROLLOFFSET;
+		//left = (idx - 1)/2 * my.TAB_WIDTH + (my.TAB_MARGIN * 2) + my.TAB_WIDTH;
+	}
+	
+	if (idx == 0) {
+		tab.addClass('first_tab');
+		top = 0;
+	}
+	else {
+		tab.removeClass('first_tab');
+	}	
+
+    tab.css('left', left);
+    tab.css('top', top);
+    
     /* If this is the active tab, we make sure it is visible. */
     if(my.active === tab_id) {
       var tabs_width = Object.keys(my.tabs_divs).length * (my.TAB_WIDTH + my.TAB_MARGIN);
@@ -306,6 +336,48 @@ var strip_c = function(spec, my) {
     var tab = my.tabs_divs[tab_id];
     delete my.tabs_divs[tab_id];
     tab.remove();
+  };
+  
+  // ### update_scroll
+  //
+  // Enables or disables the scroll buttons and moves the tabs around accordingly
+  //
+  update_scroll = function() {
+	//Scroll back button
+	if (my.TAB_SCROLLOFFSET >= 0) {
+		my.scroll_back_el.addClass('disabled');
+	}
+	else {
+		my.scroll_back_el.removeClass('disabled');
+	}
+	var left = my.new_el.position().left + my.new_el[0].offsetWidth + my.scroll_back_el[0].offsetWidth;
+	my.wrapper_el.css('left', left + "px");
+	
+	//Scroll forward button
+	if (((Object.keys(my.tabs_divs).length + (Object.keys(my.tabs_divs).length % 2 ? 1 : 2))/2 * my.TAB_WIDTH) + my.TAB_SCROLLOFFSET > my.wrapper_el[0].offsetWidth) {
+		my.scroll_forward_el.removeClass('disabled');
+	} else {
+		my.scroll_forward_el.addClass('disabled');
+	}
+  };
+  
+  var scroll = function(amount) {
+	  my.TAB_SCROLLOFFSET += amount;
+	  if (my.TAB_SCROLLOFFSET > 0) {
+		  my.TAB_SCROLLOFFSET = 0;
+	  } else {
+		  var amountOfTabs = Object.keys(my.tabs_divs).length;
+		  var widthOfTabs = (amountOfTabs % 2 ? (amountOfTabs+1)/2*my.TAB_WIDTH : (amountOfTabs+2)/2*my.TAB_WIDTH);
+		  var maxOffset = -(widthOfTabs - my.wrapper_el[0].offsetWidth);
+		  if (my.TAB_SCROLLOFFSET < maxOffset) {
+			  my.TAB_SCROLLOFFSET = maxOffset;
+		  }
+      }
+      //my.tabs_el.css('padding-left', my.TAB_SCROLLOFFSET + "px");
+      //Put something here that updates the tabs
+      
+      //And update the scroll
+      update_scroll();
   };
 
   /**************************************************************************/
@@ -383,6 +455,9 @@ var strip_c = function(spec, my) {
       tabs_order.forEach(function(tab_id) {
         update_tab(tab_id, tabs_data[tab_id]);
       });
+      
+      /* Update scroll state */
+      tabs_order.forEach(update_scroll);
 
       /* Update update state. */
       if(state.breach_update) {
@@ -432,14 +507,14 @@ var strip_c = function(spec, my) {
     my.socket.emit('back');
   };
 
-  // ### cmd_back
+  // ### cmd_forward
   //
   // Issue a forward command
   cmd_forward = function() {
     my.socket.emit('forward');
   };
 
-  // ### cmd_back
+  // ### cmd_new
   //
   // Issue a new tab command
   cmd_new = function() {
@@ -452,7 +527,21 @@ var strip_c = function(spec, my) {
   cmd_update = function() {
     my.socket.emit('update');
   };
-
+  
+  // ### cmd_scroll_back
+  //
+  // Scroll the tabs back
+  cmd_scroll_back = function() {
+	  scroll(100);
+  };
+  
+  // ### cmd_scroll_forward
+  //
+  // Scroll the tabs forward
+  cmd_scroll_forward = function() {
+	  scroll(-100);
+  };
+  
   // ### init
   //
   // Initialises the controller
@@ -462,6 +551,7 @@ var strip_c = function(spec, my) {
     my.socket = io();
     my.socket.on('connect', function() {
       my.socket.on('state', state_handler);
+      my.socket.on('scroll', scroll);
       my.socket.emit('handshake', '_strip');
     });
 
@@ -476,6 +566,8 @@ var strip_c = function(spec, my) {
   that.cmd_forward = cmd_forward;
   that.cmd_new = cmd_new;
   that.cmd_update = cmd_update;
+  that.cmd_scroll_back = cmd_scroll_back;
+  that.cmd_scroll_forward = cmd_scroll_forward;
 
   that.init = init;
 
